7faf2aeaf351c4bbdced222abd90bbe344691205
anlosty <anlosty@gmail.com>
2013-01-09 14:58:06 -0800

    Add empty function

---
+++ app.js
@@ -0,0 +1,18 @@
+var startView = function () {
+	console.log("display start view");
+};
+
+var findProductView = function () {
+	console.log("display search to all product");
+};
+
+var addProductView = function () {
+	console.log("display adding product view");
+};
+
+var reportView = function () {
+	console.log("display report for today");
+};
+
+
+
